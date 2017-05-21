27e93cb4caf8371ea25b489eff30eb2eca836287
Roland Németh <roland951007@gmail.com>
2017-05-21 16:16:27 +0100

    Minor fix.

--- FaultAssist/app/src/main/java/hu/bme/iit/faultassist/IssuesList.java
+++ FaultAssist/app/src/main/java/hu/bme/iit/faultassist/IssuesList.java
@@ -3,9 +3,11 @@ package hu.bme.iit.faultassist;
 import java.util.ArrayList;
 import java.util.List;
 
+/** Used to add to a list the elements from the the issues table. **/
 public class IssuesList {
     List<IssueElement> elements = new ArrayList<>();
 
+    /** Simple function to add elements to the list. Making a IssueElement **/
     public void add (String id, String cause){
         IssueElement element = new IssueElement();
         element.id=id;
